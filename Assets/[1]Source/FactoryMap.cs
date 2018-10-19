60f933bbade74aad86a21024bc1e691aafbb1e97
Alexeee <aleksey_ohezin@mail.ru>
2018-10-19 21:06:17 +0500

    Цель достигнута
    Цель: клетки "превращать" в "здания" при их создании.

--- Assets/[1]Source/FactoryMap.cs
+++ Assets/[1]Source/FactoryMap.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Homebrew;
+using System.Collections.Generic;
 
 namespace MyProject.Map
 {
@@ -10,10 +11,16 @@ namespace MyProject.Map
         [SerializeField]
         private GameObject prefabCell;  // Префаб клетки
 
-        public void Spawn(int width, int height)
+        public void Spawn(int width, int height, int countHouse = 2)
         {
             float offsetX = width / 2;
             float offsetZ = height / 2;
+
+            // List<Point> indexHouses = RandomPoint(height, width, countHouse);
+            List<Position> indexHouses = new List<Position>(){
+                new Position(0,0),
+                new Position(3,3)
+            };
 
             for (int z = 0; z < height; z++)
             {
@@ -22,8 +29,48 @@ namespace MyProject.Map
                     // Создание игровой клетки
                     var actor = this.Populate<Actor>(Pool.Entities, prefabCell, new Vector3(x - offsetX, 0, z - offsetZ));
                     actor.selfTransform.name = "Cell z:" + z + " x:" + x;
+
+                    if (indexHouses.Count <= 0)
+                        continue;
+
+                    Position point = new Position(z, x);
+                    if (indexHouses.Contains(point))
+                    {
+                        // Добавление компонента к сущности
+                        var composer = new EntityComposer(actor.entity, 1);
+                        composer.Add<ComponentHouse>();
+
+                        composer.Deploy();
+                    }
                 }
             }
+        }
+
+        // Создание списка случайных клеток
+        private List<Position> RandomPoint(int height, int width, int count)
+        {
+            List<Position> points = new List<Position>(count);
+
+            for (int i = 0; i < count; i++)
+            {
+                int rX = Random.Range(0, width);
+                int rY = Random.Range(0, height);
+
+                points.Add(new Position(rX, rY));
+            }
+            return points;
+        }
+    }
+
+// Структура позиции клетки в XZ плоскости
+    public struct Position
+    {
+        public readonly int X;
+        public readonly int Z;
+        public Position(int x, int z)
+        {
+            X = x;
+            Z = z;
         }
     }
 }
